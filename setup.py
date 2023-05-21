0cbe364148faed501d1bb8e18cb007124f36f309
aliclaz <alastair.claridge01@gmail.com>
2023-05-21 15:58:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='1.0.4',
     author='Roman Sol (ZFTurbo)',
     packages=['segmentation_models_3D', 'segmentation_models_3D/backbones', 'segmentation_models_3D/base', 'segmentation_models_3D/models'],
-    url='https://github.com/ZFTurbo/segmentation_models_3D',
+    url='https://github.com/aliclaz/segmentation_models_3D',
     description='Set of Keras models for segmentation of 3D volumes .',
     long_description='3D variants of popular models for segmentation like FPN, Unet, Linknet and PSPNet.'
                      'Models work with keras and tensorflow.keras.'
