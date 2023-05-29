c58d9854451375ff6346ceffddb3d500b20a9589
aliclaz <alastair.claridge01@gmail.com>
2023-05-29 13:17:52 +0100

    'Trying to identify where shape goes wrong'

--- segmentation_models_3D/models/atten_res_unet.py
+++ segmentation_models_3D/models/atten_res_unet.py
@@ -95,8 +95,9 @@ def AttentionBlock(x, gating, inter_shape, name=None):
     sigmoid_xg = layers.Activation('softmax')(psi)
     shape_sigmoid = backend.int_shape(sigmoid_xg)
     upsample_psi = layers.UpSampling3D(size=(shape_x[1] // shape_sigmoid[1], shape_x[2] // shape_sigmoid[2], shape_x[3] // shape_sigmoid[3]))(sigmoid_xg)
-                                             
+    print(upsample_psi.shape)                            
     upsample_psi = RepeatElement(upsample_psi, shape_x[4])
+    print(upsample_psi.shape)
 
     y = layers.multiply([upsample_psi, x])
 
